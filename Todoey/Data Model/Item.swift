4e9871fb95b6686b62661166f3a3d8d18a390060
Mathew Sayed <mathew.sayed@gmail.com>
2019-01-03 11:30:51 +1000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mathew Sayed on 3/1/19.
+//  Copyright © 2019 Oneski. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
